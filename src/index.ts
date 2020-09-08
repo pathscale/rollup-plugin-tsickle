89653416259ad23171896af8f399aba9a33e557c
Anton Kudryavtsev <boblobl4@gmail.com>
2020-09-08 22:43:58 +0300

    fix: misc fixes and improvements

--- src/index.ts
+++ src/index.ts
@@ -9,68 +9,42 @@ import * as ts from "typescript";
 import { tsToJs, humanlizePath } from "./utils";
 
 export default (options: Options = {}): Plugin => {
-  const isIncluded = createFilter(
-    options.include ?? ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
-    options.exclude,
-  );
+  const isIncluded = createFilter(options.include ?? /\.tsx?$/i, options.exclude);
+  const tsconfig = options.tsconfig ?? "tsconfig.json";
 
   const zip = new JSZip();
   const emitted = new Map<string, string>();
-  const tsconfig = "tsconfig.json";
 
-  // TODO: Externs are broken
-  // const externDir = "dist/externs";
-  // const externFile = path.resolve(externDir, "externs.js");
-  // fs.ensureDirSync(externDir);
+  let compilerOptions: ts.CompilerOptions;
+  let compilerHost: ts.CompilerHost;
+  let tsickleHost: tsickle.TsickleHost;
 
   const plugin: Plugin = {
     name: "tsickle",
+
+    buildStart() {
+      const loaded = ts.readConfigFile(tsconfig, file => fs.readFileSync(file, "utf8"));
+      const parsed = ts.parseJsonConfigFileContent(loaded.config, ts.sys, ".", {}, tsconfig);
+      compilerOptions = parsed.options;
+      compilerHost = ts.createCompilerHost(compilerOptions);
+      tsickleHost = {
+        shouldSkipTsickleProcessing: () => false,
+        shouldIgnoreWarningsForPath: () => false,
+        pathToModuleName: name => name,
+        fileNameToModuleId: name => name,
+        options: compilerOptions,
+        moduleResolutionHost: compilerHost,
+        googmodule: false,
+      };
+    },
 
     transform(_, id) {
       if (!isIncluded(id)) return;
       options.debug && console.log(`TSICKLE - INCLUDED (${humanlizePath(id)})`);
 
       const sourceFileName = id.replace(/\\/g, "/");
-
-      const loaded = ts.readConfigFile(tsconfig, file => fs.readFileSync(file, "utf8"));
-
-      const { options: compilerOptions } = ts.parseJsonConfigFileContent(
-        loaded.config,
-        ts.sys,
-        ".",
-        {},
-        tsconfig,
-      );
-
-      compilerOptions.allowJs = true;
-      compilerOptions.checkJs = true;
-
-      const compilerHost = ts.createCompilerHost(compilerOptions);
       const program = ts.createProgram([sourceFileName], compilerOptions, compilerHost);
-
-      const tsickleHost: tsickle.TsickleHost = {
-        shouldSkipTsickleProcessing: () => false,
-        shouldIgnoreWarningsForPath: () => false,
-        pathToModuleName: name => name,
-        fileNameToModuleId: name => name,
-        options: compilerOptions,
-        es5Mode: false,
-        moduleResolutionHost: compilerHost,
-        googmodule: false,
-        transformDecorators: true,
-        transformTypesToClosure: true,
-        typeBlackListPaths: new Set(),
-        untyped: true,
-      };
-
-      tsickle.emitWithTsickle(
-        program,
-        tsickleHost,
-        compilerHost,
-        compilerOptions,
-        undefined,
-        (file: string, code: string) => emitted.set(file, code),
-      );
+      tsickle.emit(program, tsickleHost, (file: string, code: string) => emitted.set(file, code));
 
       const sourceFileAsJs = tsToJs(sourceFileName);
       for (const [file, source] of emitted) {
@@ -81,6 +55,8 @@ export default (options: Options = {}): Plugin => {
         options.debug && console.log(`TSICKLE - PROCESSED (${sourceRel} to ${resRel})`);
         return { code: source };
       }
+
+      options.debug && console.log(`TSICKLE - MISSED (${humanlizePath(id)})`);
 
       return null;
     },
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 import path from "path";
 
-export const tsToJs = (path: string): string => path.replace(/\.tsx?$/, ".js");
+export const tsToJs = (path: string): string => path.replace(/\.tsx?$/i, ".js");
 
 export const humanlizePath = (filepath: string): string => path.relative(process.cwd(), filepath);
